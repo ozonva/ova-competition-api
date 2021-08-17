4217bd9031002ac1dbf8cc42d5fd99a2c2eb9f73
Sukharev Andrey <shizik32@gmail.com>
2021-08-17 20:43:39 +0300

    Move inner function to a separate function

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -6,26 +6,26 @@ import (
 	"time"
 )
 
-func WriteSomeEntries(entriesCount int, filepath string) {
-	writeEntry := func(filepath string) error {
-		file, err := os.OpenFile(filepath, os.O_CREATE|os.O_APPEND, 0755)
-		if err != nil {
-			return err
-		}
-
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-				panic(err)
-			}
-		}(file)
-		entry := fmt.Sprintf("new entry at %v\n", time.Now())
-		_, _ = file.WriteString(entry)
-		return nil
+func WriteEntry(filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		return err
 	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
 
+	entry := fmt.Sprintf("new entry at %v\n", time.Now())
+	_, _ = file.WriteString(entry)
+	return nil
+}
+
+func WriteSomeEntries(entriesCount int, filepath string) {
 	for i := 0; i < entriesCount; i++ {
-		err := writeEntry(filepath)
+		err := WriteEntry(filepath)
 		if err != nil {
 			panic(err)
 		}
