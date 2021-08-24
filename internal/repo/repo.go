ccacf299fec5ba42d1f043b88aeb75eb26794c0e
Sukharev Andrey <shizik32@gmail.com>
2021-08-24 18:54:45 +0300

    Add repo and flusher with tests

---
+++ internal/repo/repo.go
@@ -0,0 +1,10 @@
+package repo
+
+import "ozonva/ova-competition-api/internal/models"
+
+// Repo - интерфейс хранилища для сущности Competition
+type Repo interface {
+	AddEntities(entities []models.Competition) error
+	ListEntities(limit, offset uint64) ([]models.Competition, error)
+	DescribeEntity(entityId uint64) (*models.Competition, error)
+}
