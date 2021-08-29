61f43709f29f4dc57f8e4e24f219006340703545
Sukharev Andrey <SukharevAndrey@users.noreply.github.com>
2021-08-29 16:58:19 +0300

    Add saver

---
+++ internal/saver/saver.go
@@ -0,0 +1,109 @@
+package saver
+
+import (
+	"errors"
+	"fmt"
+	"ozonva/ova-competition-api/internal/flusher"
+	"ozonva/ova-competition-api/internal/models"
+	"sync"
+	"time"
+)
+
+type saver struct {
+	m              sync.Mutex
+	ticker         *time.Ticker
+	interval       time.Duration
+	buffer         []models.Competition
+	flusher        flusher.Flusher
+	capacity       uint
+	tickerStopChan chan struct{}
+}
+
+type Saver interface {
+	Save(competition models.Competition) error
+	Close() error
+}
+
+// NewSaver возвращает Saver с поддержкой периодического сохранения
+func NewSaver(
+	capacity uint,
+	interval time.Duration,
+	flusher flusher.Flusher,
+) Saver {
+	s := &saver{
+		capacity: capacity,
+		interval: interval,
+		buffer:   make([]models.Competition, 0, capacity),
+		flusher:  flusher,
+	}
+	s.init()
+	return s
+}
+
+func (s *saver) init() {
+	s.ticker = time.NewTicker(s.interval)
+	s.tickerStopChan = make(chan struct{})
+
+	var wg *sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			select {
+			case _, ok := <-s.tickerStopChan:
+				if !ok {
+					s.ticker.Stop()
+					return
+				}
+			case <-s.ticker.C:
+				err := s.flush()
+				if err != nil {
+					return
+				}
+				break
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+func (s *saver) flush() error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if len(s.buffer) > 0 {
+		err := s.flusher.Flush(s.buffer)
+		if err != nil {
+			return errors.New(fmt.Sprintf("failed to flush buffer: %v", err))
+		}
+		s.buffer = make([]models.Competition, 0, s.capacity)
+	}
+
+	return nil
+}
+
+func (s *saver) Save(competition models.Competition) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if len(s.buffer) >= int(s.capacity) {
+		return errors.New(fmt.Sprintf("error saving competition %v", competition))
+	}
+
+	s.buffer = append(s.buffer, competition)
+	return nil
+}
+
+func (s *saver) Close() error {
+	err := s.flush()
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to close saver: %v", err))
+	}
+
+	s.tickerStopChan <- struct{}{}
+	close(s.tickerStopChan)
+	return nil
+}
